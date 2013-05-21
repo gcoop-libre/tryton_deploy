e9c9c7a4e601ef43dd6af9835450de9440c5db18
Joaquín Sorianello <joac@gcoop.coop>
2013-05-21 15:07:32 -0300

    Default password handling

--- tryton_bootstrap.py
+++ tryton_bootstrap.py
@@ -1,6 +1,6 @@
 #! -*- coding: utf8 -*-
 
-"""install tryton in a custom instance tryton modules"""
+"""install tryton in a custom instance and init tryton modules"""
 
 import warnings
 
@@ -9,14 +9,24 @@ from proteus import config, Model, Wizard
 
 CONFIG_FILE = '/home/tryton/runtime/trytond.conf'
 LANGUAGE = 'es_AR'
+DEFAULT_PASSWORD = 'admin'
+DEFAULT_DB_NAME = 'default'
 
-db_name = raw_input('Ingrese el nombre de la base de datos [default]: ')
+db_name = raw_input(
+    'Ingrese el nombre de la base de datos [%s]: '
+    % DEFAULT_DB_NAME
+    )
+
 if not db_name:
-    db_name = 'default'
+    db_name = DEFAULT_DB_NAME
 
 password = getpass.getpass(
-    "Ingrese el password de admin a utilizar en la base '%s': " % db_name
+    "Ingrese el password de admin a utilizar en la base '%s' [%s]: "
+    % (db_name, DEFAULT_PASSWORD)
     )
+
+if not password:
+    password = DEFAULT_PASSWORD
 
 #if database doesn't exists, proteus creates it
 config = config.set_trytond(
